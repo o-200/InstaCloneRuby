deb3df40c1c4164facc9594022f62defbbf2d323
Alexander Abramov <omgout200@gmail.com>
2023-01-06 23:33:53 +0700

    added sign_out

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  root 'posts#index'
   devise_for :users
+
+  resources :users do
+    resources :followers_relationships, only: [:create]
+  end
+
+  resources :followers_relationships, only: [:destroy]
 
   resources :posts do
     resources :likes
     resources :comments
   end
-
-  root 'posts#index'
 end
